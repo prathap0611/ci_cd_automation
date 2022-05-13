3e6f15c83325e9a11210b22eb6be03e02abc4922
Hari Prathap <hari.prathap@cloudsensesolutions.com>
2022-05-13 10:00:13 +0530

    Add helm charts

--- services/auth/src/app.js
+++ services/auth/src/app.js
@@ -3,9 +3,14 @@ const app = express()
 const port = 3001
 
 app.get('/', (req, res) => {
-  res.send('I am auth service')
+    res.send('I am auth service')
+})
+
+app.get('/auth', (req, res) => {
+    console.log("Hitting auth endpoint");
+    res.sendStatus(401);
 })
 
 app.listen(port, () => {
-  console.log(`Auth service listening on port ${port}`)
+    console.log(`Auth service listening on port ${port}`)
 })
